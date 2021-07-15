da1946f9defd5d9f223fa10b012f96afc2cae2e0
Nageswara Rao <nageshraoj@gmail.com>
2021-07-15 13:53:05 +0530

    corrected user data

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "demoec2" {
   user_data              = <<EOF
     #!/bin/bash
     sudo yum update -y 
-    sudo yum install -y
+    sudo yum install httpd -y
     sudo systemctl start httpd
     sudo systemctl enable httpd
   EOF
